b6e851396bb222b75115bd36f18e1754768140da
alexisbosman <bosman.alexis@gmail.com>
2016-11-24 16:53:09 +0100

    buildprop.sh: remove lines for now

--- buildprop.sh
+++ buildprop.sh
@@ -1,21 +1,5 @@
 #!/sbin/sh
 
-sed -i '/ro.kernel.android.checkjni/d' >> /system/build.prop
-sed -i '/ro.kernel.checkjni/d' >> /system/build.prop
-sed -i '/debugtool.anrhistory/d' >> /system/build.prop
-sed -i '/profiler.debugmonitor/d' >> /system/build.prop
-sed -i '/profiler.launch/d' >> /system/build.prop
-sed -i '/profiler.hung.dumpdobugreport/d' >> /system/build.prop
-sed -i '/ro.config.htc.nocheckin/d' >> /system/build.prop
-sed -i '/ro.config.nocheckin/d' >> /system/build.prop
-sed -i '/profiler.force_disable_err_rpt/d' >> /system/build.prop
-sed -i '/profiler.force_disable_ulog/d' >> /system/build.prop
-sed -i '/dalvik.vm.verify-bytecode/d' >> /system/build.prop
-sed -i '/dalvik.vm.dexopt-flags/d' >> /system/build.prop
-sed -i '/persist.debug.sf.statistics/d' >> /system/build.prop
-sed -i '/vidc.debug.level/d' >> /system/build.prop
-sed -i '/ro.sf.lcd_density/d' >> /system/build.prop
-
 echo "ro.kernel.android.checkjni=0" >> /system/build.prop
 echo "ro.kernel.checkjni=0" >> /system/build.prop
 echo "debugtool.anrhistory=0" >> /system/build.prop
